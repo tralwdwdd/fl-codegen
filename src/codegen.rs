233f285e92ec12aa594b5d69ec60a3390e1af708
tralwdwdd <tariqgamer321@gmail.com>
2024-10-07 21:49:55 +0100

    source code

---
+++ src/codegen.rs
@@ -0,0 +1,46 @@
+use hmac::{Hmac, Mac};
+use sha1::Sha1;
+
+type HmacSha1 = Hmac<Sha1>;
+
+const CODE_GRANULARITY_MS: u64 = 1000 * 60;
+
+pub fn generate_fl_code(shared_secret: String, custom_timestamp: u64) -> String {
+    let key = shared_secret.trim();
+
+    let timestamp_ms: u64 = custom_timestamp * 1000;
+
+    let code_validity_ms = 1000u64 * 60 * 60; // 1 hour
+    let interval = timestamp_ms / code_validity_ms;
+    let interval_beginning_timestamp_ms = interval * code_validity_ms;
+    let adjusted_timestamp = interval_beginning_timestamp_ms / CODE_GRANULARITY_MS;
+    let big_endian_timestamp = adjusted_timestamp.to_be_bytes();
+
+    let mut mac = HmacSha1::new_from_slice(key.as_bytes()).unwrap();
+    mac.update(&big_endian_timestamp[..]);
+    let digest = mac.finalize().into_bytes();
+
+    let offset = (digest.last().expect("digest is not empty") & 0xf) as usize;
+    // read as i32 and clear sign bit
+    let result = i32::from_be_bytes(digest[offset..][..4].try_into().unwrap()) & 0x7fffffff;
+
+
+
+    let code: i32 = result % 1000000;
+    format!("{:06}", code)
+}
+
+pub fn remaining_time(custom_timestamp: u64) -> u64 {
+    let timestamp_ms: u64 = custom_timestamp * 1000;
+
+    let code_validity_ms = 1000u64 * 60 * 60; // 1 hour
+    let interval = timestamp_ms / code_validity_ms;
+    let interval_beginning_timestamp_ms = interval * code_validity_ms;
+
+    let valid_from_ms = interval_beginning_timestamp_ms;
+    let valid_to_ms = valid_from_ms + code_validity_ms;
+
+    let remaining_sec = (valid_to_ms - timestamp_ms) / 1000;
+
+    remaining_sec
+}
---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+mod codegen;
+use codegen::{generate_fl_code, remaining_time};
+use clap::{Arg, Command};
+
+
+fn main() {
+    let matches = Command::new("fl-codegen")
+        .version("1.0")
+        .about("Generates a Family Link code and shows remaining time")
+        .bin_name("fl-codegen")
+        .arg(
+            Arg::new("shared_code")
+                .help("Shared code to generate Family Link code")
+                .required(true)
+                .index(1)
+        )
+        .arg(
+            Arg::new("timestamp")
+                .short('t')
+                .long("timestamp")
+                .help("Optional UNIX timestamp to use instead of current time")
+                .num_args(1)
+        )
+        .get_matches();
+
+    
+    let shared_code = matches.get_one::<String>("shared_code").unwrap().clone();
+
+    
+    let curr_time_secs = matches
+        .get_one::<String>("timestamp")
+        .map(|t| t.parse().unwrap_or_else(|_| {
+            eprint!("Error: Invalid timestamp specified");
+            std::process::exit(1);
+        }))
+        .unwrap_or_else(|| SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs());
+
+    
+    println!("Family Link Code: {}", generate_fl_code(shared_code, curr_time_secs));
+    print!(
+        "Time left: {:02}:{:02}",
+        remaining_time(curr_time_secs) / 60,
+        remaining_time(curr_time_secs) % 60
+    );
+}
